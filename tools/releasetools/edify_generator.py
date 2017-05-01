8d83a95136ebfb0258b7a8c53d61487870eab41c
tuxafgmur <tuxafgmur@users.noreply.github.com>
2017-05-02 01:39:14 +0200

    Deletes recovery into system partition on install
    Conditionally, when the DELETE_RECOVERY environment variable is set to 'true'.

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -165,6 +165,12 @@ class EdifyGenerator(object):
   def RunBackup(self, command):
     self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s");' % command))
 
+  def DeleteRecovery(self, command):
+    self.script.append('delete("/system/recovery-from-boot.p");')
+    self.script.append('delete("/system/etc/recovery-resource.dat");')
+    self.script.append('delete("/system/etc/recovery.img");')
+    self.script.append('delete("/system/bin/install-recovery.sh");')
+    
   def ValidateSignatures(self, command):
     self.script.append('package_extract_file("META-INF/org/lineageos/releasekey", "/tmp/releasekey");')
     # Exit code 124 == abort. run_program returns raw, so left-shift 8bit
